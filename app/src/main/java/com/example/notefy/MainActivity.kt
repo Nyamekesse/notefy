65ddab81e9537c4d74a8beb01fb13387b659b32b
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-05-18 22:42:20 +0000

    Completed NoteScreen added Inout component, save button and TopAppBar to Note screen

--- app/src/main/java/com/example/notefy/MainActivity.kt
+++ app/src/main/java/com/example/notefy/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.notefy.screens.NoteScreen
 import com.example.notefy.ui.theme.NoteFyTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello there")
+                NoteScreen()
             }
         }
     }
