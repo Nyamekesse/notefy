052d989ce8388a9d07aa9fccb05dde3ec27cb908
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-05-18 20:28:30 +0000

    Initial commit, setup project

---
+++ app/src/main/java/com/example/notefy/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.notefy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.notefy.ui.theme.NoteFyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                Text(text = "Hello there")
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    NoteFyTheme {
+        content()
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApp {
+        Text(text = "Hello there")
+    }
+}
