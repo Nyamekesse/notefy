7589db8aea03136d573ebf5aea4d824990cb31cb
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-05-26 16:57:13 +0000

    Added TypeConverter to convert the date object to compatible type to be used by room database.

---
+++ app/src/main/java/com/example/notefy/Converters.kt
@@ -0,0 +1,16 @@
+package com.example.notefy
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+object Converters {
+    @TypeConverter
+    fun fromDate(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun toDate(timestamp: Long?): Date? {
+        return timestamp?.let { Date(it) }
+    }
+}
